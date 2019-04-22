7d2f543b0b6456b6dde96b5db21c279481591ab6
Tiago Ferrer <ferrertiago@gmail.com>
2019-04-22 18:18:36 -0300

    formatando CNPJ

--- src/main/java/com/use3w/mng/model/Supplier.java
+++ src/main/java/com/use3w/mng/model/Supplier.java
@@ -5,6 +5,10 @@ import org.hibernate.validator.constraints.br.CNPJ;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.swing.text.MaskFormatter;
+import java.text.ParseException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Entity
 public class Supplier {
@@ -26,7 +30,7 @@ public class Supplier {
     public Supplier() {
     }
 
-    public Supplier(String supplierCNPJ, Integer supplierId,String supplierName, String supplierEmail) {
+    public Supplier(String supplierCNPJ, Integer supplierId, String supplierName, String supplierEmail) {
         this.supplierCNPJ = supplierCNPJ;
         this.supplierId = supplierId;
         this.supplierName = supplierName;
@@ -34,6 +38,18 @@ public class Supplier {
     }
 
     public String getSupplierCNPJ() {
+        try {
+            System.out.println(supplierCNPJ);
+            MaskFormatter mask = new MaskFormatter("###.###.###/####-##");
+            mask.setValueContainsLiteralCharacters(false);
+            supplierCNPJ = mask.valueToString(supplierCNPJ);
+            System.out.println(supplierCNPJ);
+
+        } catch (ParseException ex) {
+            Logger.getLogger(Product.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Deu ruim!");
+        }
+
         return supplierCNPJ;
     }
 
@@ -87,5 +103,25 @@ public class Supplier {
 
     public void setSupplierEmail(String supplierEmail) {
         this.supplierEmail = supplierEmail;
+    }
+
+    public String getSupllierCNPJFormatted() {
+
+        String CNPJFormatted = this.supplierCNPJ;
+
+        System.out.println("Entrada do CNPJ" + CNPJFormatted);
+
+        try {
+            MaskFormatter mask = new MaskFormatter("###.###.###/####-##");
+            mask.setValueContainsLiteralCharacters(false);
+            CNPJFormatted = mask.valueToString(CNPJFormatted);
+
+            System.out.println("Saída do CNPJ" + CNPJFormatted);
+
+        } catch (ParseException ex) {
+            Logger.getLogger(Product.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Deu ruim!");
+        }
+        return CNPJFormatted;
     }
 }
